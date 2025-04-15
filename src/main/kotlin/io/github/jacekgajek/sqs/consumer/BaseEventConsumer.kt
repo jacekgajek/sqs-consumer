682c1c89a02df61ae907201cd14c13904b7ed9d7
jacek.gajek <jacek.gajek@staffbase.com>
2025-04-15 16:35:55 +0200

    Handle QueueNotFoundException with retry and delay
    Added a specific case for `QueueNotFoundException` in the retry logic to log an error and delay the retry by 10 seconds. This prevents immediate retries and improves robustness when the queue is missing.

--- src/main/kotlin/io/github/jacekgajek/sqs/consumer/BaseEventConsumer.kt
+++ src/main/kotlin/io/github/jacekgajek/sqs/consumer/BaseEventConsumer.kt
@@ -4,6 +4,7 @@ import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.retry
@@ -13,6 +14,7 @@ import kotlinx.serialization.serializer
 import kotlin.reflect.KClass
 import kotlin.reflect.full.createType
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
 private val log = KotlinLogging.logger { }
 
@@ -46,8 +48,13 @@ abstract class BaseEventConsumer<T : Any> (
 					log.debug { "Finished processing ${dataClazz.simpleName} event." }
 				}
 				.retry { ex ->
-					log.error(ex) { "Exception during processing a message in queue [$queueName]. The message won't be deleted from the queue. [${ex.message}]." }
-					true
+                    if (ex is QueueNotFoundException) {
+                        log.error { "Cannot start consumer: queue [$queueName] was not found. Retrying in 10 seconds..." }
+                        delay(10.seconds)
+                    } else {
+                        log.error(ex) { "Exception during processing a message in queue [$queueName]. The message won't be deleted from the queue. [${ex.message}]." }
+                    }
+                    true
 				}
 				.collect()
 		}
